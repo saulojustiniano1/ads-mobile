0b3698b61723ed003397584afd643382a19dee3a
saulojustiniano1 <saulojustiniano1@gmail.com>
2024-09-19 12:58:45 -0300

    Refactor Main.kt to add new functions for filtering products by category and discount

--- kotlin/aulas/kotlin-poo-funcional/src/main/Main.kt
+++ kotlin/aulas/kotlin-poo-funcional/src/main/Main.kt
@@ -8,20 +8,29 @@ fun main() {
 
     printProductsByCategory("Video Games", products)
     printProductsByPrice(8000f, products)
+
+    printProductsByDiscount(products) // Todos os produtos com desconto
+    printProcuctsByCatedoryVideoGames(products) // Produtos na categoria Video Games
+    printProcuctsByCatedoryComputers(products) // Produtos na categoria Computers
+    printProcuctsByCatedorySmartphones(products) // Produtos na categoria Smartphones
+    printProcuctsByCatedoryCameras(products) // Produtos na categoria Cameras
+
 }
 
 fun createProducts(): List<Product> {
     return listOf(
-        Product(1, "Xbox", 2000f, "Video Games"),
-        Product(2, "Playstation", 2200f, "Video Games"),
-        Product(3, "Nintendo Switch", 1800f, "Video Games"),
+        Product(1, "Xbox", 2000f, "Video Games", 10f),
+        Product(2, "Playstation", 2200f, "Video Games", 10f),
+        Product(3, "Nintendo Switch", 1800f, "Video Games", 20f),
         Product(4, "Macbook Pro", 10000f, "Computers"),
         Product(5, "Dell XPS", 8000f, "Computers"),
         Product(6, "Lenovo Thinkpad", 7000f, "Computers"),
-        Product(7, "Iphone 12", 8000f, "Smartphones"),
-        Product(8, "Samsung Galaxy S21", 7000f, "Smartphones"),
-        Product(9, "Oneplus 9", 6000f, "Smartphones"),
+        Product(7, "Iphone 12", 8000f, "Smartphones", 10f),
+        Product(8, "Samsung Galaxy S21", 7000f, "Smartphones", 10f),
+        Product(9, "Oneplus 9", 6000f, "Smartphones", 20f),
         Product(10, "Canon EOS R5", 15000f, "Cameras"),
+        Product(11, "Sony A7 III", 12000f, "Cameras"),
+        Product(12, "Nikon Z6", 10000f, "Cameras")
     )
 }
 
@@ -32,7 +41,7 @@ fun printProducts(products: List<Product>) {
 }
 
 fun printProductsByCategory(category: String, products: List<Product>) {
-    println("Products in category $category")
+    println("Produtos na categoria $category")
     val result = products.filter { p ->
         p.category == category
     }
@@ -40,9 +49,49 @@ fun printProductsByCategory(category: String, products: List<Product>) {
 }
 
 fun printProductsByPrice(price: Float, products: List<Product>) {
-    println("Products with price less than $price")
+    println("Produtos com preço menor que $price")
     val result = products.filter { p ->
         p.price >= price
+    }
+    printProducts(result)
+}
+
+fun printProductsByDiscount(products: List<Product>) {
+    println("Todos os produtos com desconto")
+    val result = products.filter { p ->
+        p.discount > 0
+    }
+    printProducts(result)
+}
+
+fun printProcuctsByCatedoryVideoGames(products: List<Product>) {
+    println("Produtos na categoria Video Games")
+    val result = products.filter { p ->
+        p.category == "Video Games"
+    }
+    printProducts(result)
+}
+
+fun printProcuctsByCatedoryComputers(products: List<Product>) {
+    println("Produtos na categoria Computers")
+    val result = products.filter { p ->
+        p.category == "Computers"
+    }
+    printProducts(result)
+}
+
+fun printProcuctsByCatedorySmartphones(products: List<Product>) {
+    println("Produtos na categoria Smartphones")
+    val result = products.filter { p ->
+        p.category == "Smartphones"
+    }
+    printProducts(result)
+}
+
+fun printProcuctsByCatedoryCameras(products: List<Product>) {
+    println("Produtos na categoria Cameras")
+    val result = products.filter { p ->
+        p.category == "Cameras"
     }
     printProducts(result)
 }
--- kotlin/aulas/kotlin-poo-funcional/src/model/Product.kt
+++ kotlin/aulas/kotlin-poo-funcional/src/model/Product.kt
@@ -4,9 +4,14 @@ data class Product(
     val id: Int,
     val description: String,
     val price: Float = 0.0f,
-    val category: String = ""
+    val category: String = "",
+    val discount: Float = 0.0f
 ) {
     override fun toString(): String {
-        return  "#: $id - $description - $price - $category"
+        if (discount > 0) {
+            val discountedPrice = price * (1 - discount / 100)
+            return "#: $id - $description (PROMOÇÃO) - R$$price - $category - $discount% - R$$discountedPrice"
+        }
+        return  "#: $id - $description - $price - $category - $discount%"
     }
 }
